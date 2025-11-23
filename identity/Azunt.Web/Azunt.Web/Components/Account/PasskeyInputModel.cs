c4d794c502d9c13e0e455c1599805d277531a29c
Park YongJune <redplus@redplus.net>
2025-11-24 06:19:22 +0900

    ASP.NET Core Identity with Blazor Server project

---
+++ identity/Azunt.Web/Azunt.Web/Components/Account/PasskeyInputModel.cs
@@ -0,0 +1,8 @@
+namespace Azunt.Web.Components.Account
+{
+    public class PasskeyInputModel
+    {
+        public string? CredentialJson { get; set; }
+        public string? Error { get; set; }
+    }
+}
