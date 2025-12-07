e8c45972d7c7d0fa9dc646f5848249e1dad07918
Park YongJune <redplus@redplus.net>
2025-12-08 02:02:06 +0900

    Add Device class file

---
+++ src/Azunt.Web/Azunt.Web/Models/Device.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Azunt.Web.Models
+{
+    /// <summary>
+    /// Devices 
+    /// </summary>
+    public class Device
+    {
+        public int Id { get; set; }
+        public string? ModelName { get; set; }
+        public string? ModelType { get; set; }
+        public string? DeviceType { get; set; }
+        public string? DeviceId { get; set; }
+        public string? Maker { get; set; }
+        public string? UserRef { get; set; }
+        public long UnitPrice { get; set; }
+        public DateTimeOffset CreatedDate { get; set; }
+    }
+}
