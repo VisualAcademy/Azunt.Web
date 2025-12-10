3a821b769fe3a74b23fa4758b9569290c7434128
Park YongJune <redplus@redplus.net>
2025-12-11 02:39:55 +0900

    Add InvoiceNumberSequences table

---
+++ src/Azunt.Web/Azunt.SqlServer/dbo/Tables/InvoiceNumberSequences.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[InvoiceNumberSequences]
+(
+    [TenantId] NVARCHAR(64) NOT NULL,
+    [NextValue] BIGINT NOT NULL,
+    CONSTRAINT PK_InvoiceNumberSequences PRIMARY KEY CLUSTERED ([TenantId] ASC)
+);
+GO
