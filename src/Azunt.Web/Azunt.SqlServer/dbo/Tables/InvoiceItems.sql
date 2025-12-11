08db5c7dbe5fad4d8ceb23f747283048327d0d10
Park YongJune <redplus@redplus.net>
2025-12-12 02:41:26 +0900

    Add InvoiceItems table

---
+++ src/Azunt.Web/Azunt.SqlServer/dbo/Tables/InvoiceItems.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE [dbo].[InvoiceItems]
+(
+    [Id] BIGINT IDENTITY(1,1) NOT NULL,
+    [InvoiceId] BIGINT NOT NULL,
+    [Description] NVARCHAR(200) NOT NULL,
+    [Quantity] DECIMAL(18,2) NOT NULL CONSTRAINT DF_InvoiceItems_Quantity DEFAULT (1),
+    [UnitPrice] DECIMAL(18,2) NOT NULL CONSTRAINT DF_InvoiceItems_UnitPrice DEFAULT (0),
+    CONSTRAINT PK_InvoiceItems PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT FK_InvoiceItems_Invoices FOREIGN KEY ([InvoiceId]) REFERENCES [dbo].[Invoices]([Id]) ON DELETE CASCADE
+);
+GO
+
+CREATE NONCLUSTERED INDEX IX_InvoiceItems_InvoiceId
+ON [dbo].[InvoiceItems] ([InvoiceId]);
+GO
