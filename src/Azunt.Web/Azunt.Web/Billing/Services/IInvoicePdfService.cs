67ced32e095a63c50afb3019b880e35737bd5c29
Park YongJune <redplus@redplus.net>
2025-12-14 03:02:00 +0900

    Add IInvoicePdfService interface

---
+++ src/Azunt.Web/Azunt.Web/Billing/Services/IInvoicePdfService.cs
@@ -0,0 +1,8 @@
+﻿using Azunt.Web.Billing.Domain;
+
+namespace Azunt.Web.Billing.Services;
+
+public interface IInvoicePdfService
+{
+    Task<byte[]> GenerateInvoicePdfAsync(Invoice invoice, Customer customer);
+}
