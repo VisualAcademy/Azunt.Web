8108b071e04ccc22a32b442ad39d264e7129bdc0
Park YongJune <redplus@redplus.net>
2025-12-14 02:58:58 +0900

    Add IFileStorage interface

---
+++ src/Azunt.Web/Azunt.Web/Billing/Services/IFileStorage.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace Azunt.Web.Billing.Services;
+
+public interface IFileStorage
+{
+    Task<string> SaveAsync(string fileName, byte[] bytes, string contentType = "application/pdf");
+    string GetPublicUrl(string filePath);
+}
