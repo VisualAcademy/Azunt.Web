2f58a1e2b4592bed11741e50dbb86dca3ad70fdd
Park YongJune <redplus@redplus.net>
2025-12-14 02:54:55 +0900

    Add IEmailSender class

---
+++ src/Azunt.Web/Azunt.Web/Billing/Services/IEmailSender.cs
@@ -0,0 +1,9 @@
+﻿using Azunt.Web.Billing.Domain;
+using System.Threading.Tasks;
+
+namespace Azunt.Web.Billing.Services;
+
+public interface IEmailSender
+{
+    Task<bool> SendInvoiceEmailAsync(Invoice invoice, Customer customer, byte[] pdfBytes, string viewLink);
+}
