0e726a09b96b6c7238a45ae2f09de6426f774a86
Park YongJune <redplus@redplus.net>
2026-01-05 23:33:43 +0900

    Add StripePaymentsController class file

---
+++ src/Azunt.Web/Azunt.Web/Controllers/StripePaymentsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Azunt.Web.Controllers
+{
+    public class StripePaymentsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
