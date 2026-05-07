63f868150392a2aa22f95807d8657edd334b051b
Park YongJune <redplus@redplus.net>
2026-05-07 22:46:58 +0900

    Convert to file-scoped namespace

--- src/Azunt.Web/Azunt.Web/Models/Device.cs
+++ src/Azunt.Web/Azunt.Web/Models/Device.cs
@@ -1,18 +1,17 @@
-﻿namespace Azunt.Web.Models
+﻿namespace Azunt.Web.Models;
+
+/// <summary>
+/// Devices 
+/// </summary>
+public class Device
 {
-    /// <summary>
-    /// Devices 
-    /// </summary>
-    public class Device
-    {
-        public int Id { get; set; }
-        public string? ModelName { get; set; }
-        public string? ModelType { get; set; }
-        public string? DeviceType { get; set; }
-        public string? DeviceId { get; set; }
-        public string? Maker { get; set; }
-        public string? UserRef { get; set; }
-        public long UnitPrice { get; set; }
-        public DateTimeOffset CreatedDate { get; set; }
-    }
+    public int Id { get; set; }
+    public string? ModelName { get; set; }
+    public string? ModelType { get; set; }
+    public string? DeviceType { get; set; }
+    public string? DeviceId { get; set; }
+    public string? Maker { get; set; }
+    public string? UserRef { get; set; }
+    public long UnitPrice { get; set; }
+    public DateTimeOffset CreatedDate { get; set; }
 }
